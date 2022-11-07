fb841b8d4dc63758004f369e2dc1435b697dff76
Nasim-Alizai <sayedhakimi449@gmail.com>
2022-11-07 17:42:23 +0430

    changes

--- src/features/resturants/screens/resturants.screen.js
+++ src/features/resturants/screens/resturants.screen.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
 import {
 	StatusBar,
@@ -12,33 +12,28 @@ import {
 import { ResturantInfoCard } from "../components/resturant-info-card.component";
 
 const isAndroid = Platform.OS === "android";
+const SafeArea = styled.SafeAreaView`
+	flex: 1;
+	margin-top: ${isAndroid ? StatusBar.currentHeight : "0"};
+`;
+const SearchBarView = styled.View`
+	padding: 10px;
+`;
+const SearchBar = styled(Searchbar)`
+	height: 35px;
+`;
+const ResturantList = styled.View`
+	flex: 1;
+	padding: 16px;
+`;
 
 export const ResturantsScreen = () => (
-	<>
-		<SafeAreaView style={styles.container}>
-			<View style={styles.search}>
-				<Searchbar style={styles.searchbar} placeholder='Search' />
-			</View>
-			<View style={styles.list}>
-				<ResturantInfoCard />
-			</View>
-		</SafeAreaView>
-	</>
+	<SafeArea>
+		<SearchBarView>
+			<SearchBar placeholder='Search' />
+		</SearchBarView>
+		<ResturantList>
+			<ResturantInfoCard />
+		</ResturantList>
+	</SafeArea>
 );
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginTop: isAndroid ? StatusBar.currentHeight : 0,
-	},
-	search: {
-		padding: 10,
-	},
-	list: {
-		flex: 1,
-		padding: 16,
-	},
-	searchbar: {
-		height: 35,
-	},
-});
