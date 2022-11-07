174a9f9ad34c9be774d1f137b7aa284b98652a80
Nasim-Alizai <sayedhakimi449@gmail.com>
2022-11-07 18:36:29 +0430

    migrate styled components

--- src/features/resturants/screens/resturants.screen.js
+++ src/features/resturants/screens/resturants.screen.js
@@ -1,20 +1,12 @@
 import React from "react";
 import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
-import {
-	StatusBar,
-	StyleSheet,
-	Text,
-	View,
-	SafeAreaView,
-	Platform,
-} from "react-native";
+import { StatusBar } from "react-native";
 import { ResturantInfoCard } from "../components/resturant-info-card.component";
 
-const isAndroid = Platform.OS === "android";
 const SafeArea = styled.SafeAreaView`
 	flex: 1;
-	margin-top: ${isAndroid ? StatusBar.currentHeight : "0"};
+	margin-top: ${StatusBar.currentHeight}px;
 `;
 const SearchBarView = styled.View`
 	padding: 10px;
